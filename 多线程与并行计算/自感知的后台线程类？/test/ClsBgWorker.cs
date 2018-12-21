b61e1b184078041cce0076a612b252bc42291f2c
Junenatte-and-little-pig <sy5622_5@126.com>
2018-12-21 11:13:09 +0800

    cs rewrite vb programs

---
+++ 多线程与并行计算/自感知的后台线程类？/test/ClsBgWorker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace test {
+    struct WorkRange {
+        public int id;
+        public int nStart;
+        public int nEnd;
+    }
+    struct WorkResult {
+        public int id;
+        public int result;
+    }
+    class ClsBgWorker {
+        public BackgroundWorker BgWorker { get; set; }
+        private WorkRange Range { get; set; }
+        private Form Frm { get; set; }
+
+        public delegate void CompleteHandler(bool cancel, WorkResult result);   //提供一类相似方法的模板
+        public delegate void MessageHandler(int id, string msg);
+        public event CompleteHandler Complete;  //提供一定模板下的方法供具体实现
+        public event MessageHandler SendMessage;
+        public ClsBgWorker(WorkRange wr,Form frm) {
+            Range = wr;
+            Frm = frm;
+            BgWorker = new BackgroundWorker();
+            BgWorker.WorkerSupportsCancellation = true;
+            BgWorker.DoWork += new DoWorkEventHandler(DoWork);
+            BgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(RunWorkerCompleted);
+        }
+        public void Run() {
+            BgWorker.RunWorkerAsync(Range);
+        }
+        public void Cancel() {
+            BgWorker.CancelAsync();
+        }
+        private void DoWork(object sender, DoWorkEventArgs e) {
+            int sum = 0;
+            for (int k = 0; k < 1000; k++) {
+                if (BgWorker.CancellationPending) {    //指示应用程序是否已请求取消后台操作
+                    e.Cancel = true;
+                    return;
+                }
+                for (int i = Range.nStart; i < Range.nEnd; i++)
+                    sum += i;
+                //在拥有控件的基础窗口句柄的线程上，用指定的自变量列表执行指定委托
+                try {
+                    Frm.Invoke(SendMessage, Range.id, sum.ToString() + '\t' + k.ToString());
+                } catch (Exception) {
+                    return;
+                }
+            }
+            WorkResult result = new WorkResult {
+                id = Range.id,
+                result = sum
+            };
+            e.Result = result;
+        }
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            WorkResult result = (WorkResult)e.Result;
+            Complete(e.Cancelled, result);
+        }
+    }
+}
---
+++ 多线程与并行计算/自感知的后台线程类？/test/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace test {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private ClsBgWorker bgWork;
+        static int id=0;
+        private void btnStart_Click(object sender, EventArgs e) {
+            WorkRange wr = new WorkRange();
+            id = id == 0 ? 1 : 0;
+            wr.id = id;
+            wr.nStart = new Random().Next(1,1000)*100;
+            wr.nEnd = new Random().Next(1,1000) * 1000;
+            bgWork = new ClsBgWorker(wr,this);
+            bgWork.Complete += new ClsBgWorker.CompleteHandler(DoComplete);
+            bgWork.SendMessage += new ClsBgWorker.MessageHandler(SendMessage);
+            if (id == 1) {
+                lstRange.Items.Clear();
+            } else if (id == 0) {
+                lstResult.Items.Clear();
+            }
+            bgWork.Run();
+        }
+        private void btnCancel_Click(object sender, EventArgs e) {
+            bgWork.Cancel();
+        }
+        private void DoComplete(bool cancel,WorkResult result) {
+            if (cancel)
+                return;
+            if (id == 1) {
+                lstRange.Items.Add(result.result.ToString());
+            } else if (id == 0) {
+                lstResult.Items.Add(result.result.ToString());
+            }
+        }
+        private void SendMessage(int id,string msg) {
+            if (id == 1) {
+                lstRange.Items.Add(msg);
+            }else if (id == 0) {
+                lstResult.Items.Add(msg);
+            }
+        }
+    }
+}
